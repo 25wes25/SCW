705c39086a496739b06d8aa4bd1f675cbbe686d6
25wes25 <wesleyswanson97@gmail.com>
2023-02-10 19:15:58 -0800

    Updated EMAILJS Service ID

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const S3_URL =
   "https://tellico-woodworks.s3-us-east-1.amazonaws.com/public/";
-export const EMAILJS_SERVICE_ID = "service_nm3f0v8";
+export const EMAILJS_SERVICE_ID = "service_oll0muv";
 export const EMAILJS_TEMPLATE_ID = "template_9w6dx0j";
 export const EMAILJS_USER_ID = "user_bwRoqdVnotcpGaaGNQnTm";
